f9f5f1ba52e0805a03a4c43196c4b5ec23b08483
Oyvind Hansen <oyvind.hansen@outlook.com>
2020-11-10 11:02:00 -0400

    add tests

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,26 @@ const assert = require('assert');
 
 describe("Moon - Integration Tests", function () {
     describe("Moon age", function () {
-        it("it should equal", function () {
+
+        it("it should be new-moon", function () {
+            //arrange
+            const expected = 'new-moon'
+            //act
+            const actual = moon.calculate(2000, 7, 1, 19, 21, 0, 0); //1 July 2000 @ 19:21:00
+            //assert
+            assert.strictEqual(actual.name, expected);
+        });
+
+        it("it should be new-moon with time zone adjusted", function () {
+            //arrange
+            const expected = 'new-moon'
+            //act
+            const actual = moon.calculate(2020, 11, 15, 1, 7, 0, -4); //15 Nov 2020, 01:07 @ UTC-4
+            //assert
+            assert.strictEqual(actual.name, expected);
+        });
+
+        it("it should be waning-crescent", function () {
             //arrange
             const expected = {
                 age: 24.27,
